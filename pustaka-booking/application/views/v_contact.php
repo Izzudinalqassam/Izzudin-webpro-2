cc00b6226a97c1ad529f6b051b414fee444a1ad1
Izzudinalqassam <alkasam987@gmail.com>
2024-04-24 09:18:41 +0700

    Website Blog menggunakan code igniter

---
+++ pustaka-booking/application/views/v_contact.php
@@ -0,0 +1,43 @@
+<section id="contact">
+    <h1><?= $judul ?></h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title">
+                    <h2>Contact</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="contact-content">
+                    <p>Hubungi kami untuk informasi lebih lanjut atau untuk konsultasi.</p>
+                    <form action="" method="post">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <fieldset>
+                                    <input name="name" type="text" class="form-control" id="name" placeholder="Nama Anda *" required="">
+                                </fieldset>
+                            </div>
+                            <div class="col-md-6">
+                                <fieldset>
+                                    <input name="email" type="email" class="form-control" id="email" placeholder="Email Anda *" required="">
+                                </fieldset>
+                            </div>
+                            <div class="col-md-12">
+                                <fieldset>
+                                    <textarea name="message" rows="6" class="form-control" id="message" placeholder="Pesan Anda *" required=""></textarea>
+                                </fieldset>
+                            </div>
+                            <div class="col-md-12">
+                                <fieldset>
+                                    <button type="submit" id="form-submit" class="btn btn-primary">Kirim Pesan</button>
+                                </fieldset>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
